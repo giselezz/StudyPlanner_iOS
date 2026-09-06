abdd49690947d2455ff15f6dae03af245aff16ec
Yufan Zou <yufan_zou@outlook.com>
2026-09-06 22:38:54 +1000

    Initial Commit

---
+++ StudyPlanner/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StudyPlanner
+//
+//  Created by Yufan on 6/9/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StudyPlanner/StudyPlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudyPlannerApp.swift
+//  StudyPlanner
+//
+//  Created by Yufan on 6/9/2026.
+//
+
+import SwiftUI
+
+@main
+struct StudyPlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
